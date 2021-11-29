ac9babf86e6b7ee5dbe88bea01065caa5b8e78f7
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-11-29 20:03:46 +0600

    Create AminoAccountsConverter.py

---
+++ AminoAccountsConverter.py
@@ -0,0 +1,20 @@
+from pyfiglet import figlet_format
+print("""Script by deluvsushi
+Github : https://github.com/deluvsushi""")
+print(figlet_format("aminoaccountsconverter", font="small", width=57))
+
+
+def converting_process(password: str):
+    emails_txt = open("emails.txt", "r")
+    accounts_json = open("accounts.json", "a")
+    accounts_json.write("[")
+    for line in emails_txt:
+        email = line.strip()
+        accounts_json.write(
+            f'\n{{\n"email": "{email}",\n"password": "{password}"\n}},')
+    accounts_json.write("\n}\n]")
+    accounts_json.close()
+    print(f"Converted all accounts!")
+
+
+converting_process(password=input("-- Password For All Accounts::: "))
